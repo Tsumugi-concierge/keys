a4cdfb7e7d4a2ca29edb31370f1a3a6ba56a9ac5
Takahiro Hayashi <takahirohayashi@TakahironoMac-mini.local>
2026-04-26 03:24:49 +0900

    Remove temp work files

--- serve.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os, sys
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-os.chdir('/Users/takahirohayashi/Downloads/keys-pages')
-httpd = HTTPServer(('', 8766), SimpleHTTPRequestHandler)
-httpd.serve_forever()
